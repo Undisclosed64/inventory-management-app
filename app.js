4e97728a7b9fbf6c30bb055d4c37be016da4f957
Undisclosed <undisclosed674@gmail.com>
2022-03-11 04:23:25 -0800

    add store app use

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var storeRouter = require('./routes/store')
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://awesome-user:seattle@cluster0.m6wxj.mongodb.net/inventory-management?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/store',storeRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
